10e4bec2bbf09f50c8d5814ee47149fb4fa001cc
appoung <appoung@naver.com>
2021-09-22 10:26:52 +0900

    큰아버지 퀴z

---
+++ 11.py
@@ -0,0 +1,20 @@
+coins=[]
+first=int(input("가장큰코인을 입력하세요:"))
+second=int(input("두번째로 큰코인을 입력하세요:"))
+third=int(input("가장작은코인을 입력하세요:"))
+num = int(input("가격을 입력하세요:"))
+coins.append(first)
+coins.append(second)
+coins.append(third)
+for i in range(3):
+    mok=num // max(coins)
+    namugi = num % max(coins)
+    if coins[1] > namugi:
+        mok = mok + namugi // coins[2]
+    else:
+        mok = mok + namugi // coins[1]
+        namugi = namugi % coins[1]
+        mok = mok + namugi // coins[2]
+    print(mok)
+
+
